e2242b3c0c2bb04b839a124a191bd3b29feee122
Riyakuila <riyakuila1006@gmail.com>
2026-05-15 07:15:13 +0530

    Minimal UI is done

---
+++ components/JobForm.jsx
@@ -0,0 +1,156 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function JobForm({
+  fetchJobs,
+  editingJob,
+  setEditingJob,
+  formRef,
+}) {
+  const [formData, setFormData] = useState({
+    company: "",
+    role: "",
+    status: "Applied",
+    location: "",
+    salary: "",
+    appliedDate: "",
+    notes: "",
+  });
+
+  useEffect(() => {
+  if (editingJob) {
+    setFormData(editingJob);
+  }
+}, [editingJob]);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    if (editingJob) {
+      await axios.put(
+        `/api/jobs/${editingJob._id}`,
+        formData
+      );
+      setEditingJob(null);
+    } else {
+      await axios.post("/api/jobs", formData);
+    }
+
+    setFormData({
+      company: "",
+      role: "",
+      status: "Applied",
+      location: "",
+      salary: "",
+      appliedDate: "",
+      notes: "",
+    });
+
+    fetchJobs();
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+  return (
+    <form
+      ref={formRef}
+      onSubmit={handleSubmit}
+      className="bg-zinc-900 border border-zinc-800 rounded-xl p-6 mb-8"
+    >
+      <h2 className="text-2xl font-semibold mb-6">
+        {editingJob ? "Update Job Application" : "Add Job Application"}
+      </h2>
+
+      <div className="grid md:grid-cols-2 gap-4">
+
+        <input
+          type="text"
+          name="company"
+          placeholder="Company"
+          value={formData.company}
+          onChange={handleChange}
+          className="bg-zinc-800 p-3 rounded-lg outline-none"
+          required
+        />
+
+        <input
+          type="text"
+          name="role"
+          placeholder="Role"
+          value={formData.role}
+          onChange={handleChange}
+          className="bg-zinc-800 p-3 rounded-lg outline-none"
+          required
+        />
+
+        <select
+          name="status"
+          value={formData.status}
+          onChange={handleChange}
+          className="bg-zinc-800 p-3 rounded-lg outline-none"
+        >
+          <option>Applied</option>
+          <option>Interview Scheduled</option>
+          <option>Rejected</option>
+          <option>Offer Received</option>
+        </select>
+
+        <input
+          type="text"
+          name="location"
+          placeholder="Location"
+          value={formData.location}
+          onChange={handleChange}
+          className="bg-zinc-800 p-3 rounded-lg outline-none"
+        />
+
+        <input
+          type="text"
+          name="salary"
+          placeholder="Salary"
+          value={formData.salary}
+          onChange={handleChange}
+          className="bg-zinc-800 p-3 rounded-lg outline-none"
+        />
+
+        <input
+          type="date"
+          name="appliedDate"
+          value={formData.appliedDate}
+          onChange={handleChange}
+          className="bg-zinc-800 p-3 rounded-lg outline-none"
+        />
+
+      </div>
+
+      <textarea
+        name="notes"
+        placeholder="Notes"
+        value={formData.notes}
+        onChange={handleChange}
+        className="bg-zinc-800 p-3 rounded-lg outline-none w-full mt-4"
+        rows="4"
+      />
+
+      <button
+        type="submit"
+        className="bg-white text-black px-6 py-3 cursor-pointer rounded-lg mt-4 font-semibold"
+      >
+        {editingJob ? "Update Job" : "Add Job"}
+      </button>
+
+    </form>
+  );
+}
---
+++ components/JobTable.jsx
@@ -0,0 +1,75 @@
+export default function JobTable({ 
+    jobs,
+    deleteJob,
+    handleEdit,}) {
+  return (
+    <div className="bg-zinc-900 rounded-xl p-6 border border-zinc-800">
+      <h2 className="text-2xl font-semibold mb-4">
+        Applications
+      </h2>
+
+      <div className="overflow-x-auto">
+        <table className="w-full border-collapse">
+          <thead>
+            <tr className="border-b border-zinc-700">
+              <th className="text-left p-3">Company</th>
+              <th className="text-left p-3">Role</th>
+              <th className="text-left p-3">Status</th>
+              <th className="text-left p-3">Location</th>
+              <th className="text-left p-3">Salary</th>
+              <th className="text-left p-3">Applied Date</th>
+              <th className="text-left p-3">Notes</th>
+              <th className="text-left p-3">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {jobs.map((job) => (
+              <tr
+                key={job._id}
+                className="border-b border-zinc-800 hover:bg-zinc-800/40 transition"
+              >
+                <td className="p-3">{job.company}</td>
+
+                <td className="p-3">{job.role}</td>
+
+                <td className="p-3">
+                  <span className="bg-zinc-800 px-3 py-1 rounded-full text-sm">
+                    {job.status}
+                  </span>
+                </td>
+
+                <td className="p-3">{job.location}</td>
+
+                <td className="p-3">{job.salary}</td>
+
+                <td className="p-3">{job.appliedDate}</td>
+
+                <td className="p-3 max-w-[250px] truncate">
+                  {job.notes}
+                </td>
+
+                <td className="p-3 flex gap-2">
+                  <button
+                    className="bg-blue-400 px-3 py-1 rounded cursor-pointer hover:bg-blue-600 transition"
+                    onClick={() => handleEdit(job)}
+                  >
+                    Edit
+                  </button>
+
+                  <button
+                    className="bg-orange-500 px-3 py-1 rounded cursor-pointer hover:bg-orange-700 transition"
+                    onClick={() => deleteJob(job._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+      </div>
+    </div>
+  );
+}
